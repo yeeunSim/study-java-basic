f6a28959894992644f060546fe2210e826154f68
yeeunSim <wjdwlr03@gmail.com>
2025-08-21 15:33:42 +0900

    Add: 다형성3 - 1

---
+++ src/poly/car0/K3Car.java
@@ -0,0 +1,15 @@
+package poly.car0;
+
+public class K3Car {
+    public void startEngine() {
+        System.out.println("K3Car.startEngine");
+    }
+
+    public void offEnginge() {
+        System.out.println("K3Car.offEnginge");
+    }
+
+    public void pressAccelerator() {
+        System.out.println("K3Car.pressAccelerator");
+    }
+}
